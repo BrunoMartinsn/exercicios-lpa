a5e30b556423b6ce71cf64bbd8e64117833f4d24
Bruno1 <brurro67@gmail.com>
2024-03-08 10:33:57 -0300

    Exercicio 2

--- routes/api.php
+++ routes/api.php
@@ -8,3 +8,10 @@ $nome = 'bruno martins';
 return $nome;  
 
 });     
+
+Route::get( 'nome/usuario', function (Request $request){  
+    $nome = 'bruno';
+    $idade = '16';  
+    return 'meu nome é' . $nome . ", tenho" . $idade . "anos de idade"; 
+    
+    });  
